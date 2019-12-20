49bf331afb7e3768e910db5486b0b381c47b5bc0
Nour Etienne <55996357+nouretienne@users.noreply.github.com>
2019-12-19 23:23:16 -0500

    added function to add or update twitter users in twitter.py

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -4,6 +4,8 @@ from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
 
+from .twitter import add_or_update_user
+
 #make our app factory
 
 def create_app():
@@ -32,6 +34,12 @@ def create_app():
     @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
     def user(name=None, message=''):
-        pass
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} succesfully added".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            pass
 
     return app
